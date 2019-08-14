524a4dba974e27ed310110d419eadbcd918aa93a
TNT <timrousdale@gmail.com>
2019-08-13 22:33:31 -0600

    adds missing comma in sql seed

--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,6 @@
 insert into Users (first, second, email, age)
 values ('Billy', 'Bob', 'Billy@bob.com', 23),
-        ('Jane', 'Oliver' 'Jane@yahoo.com', 55),
+        ('Jane', 'Oliver', 'Jane@yahoo.com', 55),
         ('Wendy', 'Thomas', 'Wendy@aol.com', 34),
         ('Tim', 'Trousdale', 'tim@gmail.com', 34),
         ('John', 'Bon Jovi', 'John@bob.aol', 43),
